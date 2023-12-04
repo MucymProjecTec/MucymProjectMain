d6b25e5fae5482a5524cc0f844637694a82ed65d
JeanCUS <urbsiljeank@estudiantec.cr>
2023-12-03 21:04:50 -0600

    Create Hanoi Test

--- Assets/Tests/PlayMode/HanoiTest.cs
+++ Assets/Tests/PlayMode/HanoiTest.cs
@@ -1,25 +1,113 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
+using System.Collections;
+using UnityEngine;
 
+[TestFixture]
 public class HanoiTest
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void HanoiTestSimplePasses()
+    [UnityTest]
+    public IEnumerator WinCondition_CheckWinCondition_WithExpectedValue_ShouldReturnTrue()
     {
-        // Use the Assert class to test conditions
+        // Configuración de prueba
+        GameObject gameObj = new GameObject();
+        WinCondition winCondition = gameObj.AddComponent<WinCondition>();
+        HanoiDisk hanoiDisk = CreateDiskWithSize(5); // Crear un disco con el tamaño esperado
+        winCondition.expectedValue = 5;
+
+        // Ejecutar la prueba
+        bool result = winCondition.CheckWinCondition();
+
+        // Verificar el resultado
+        Assert.IsFalse(result);
+
+        yield return null;
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator HanoiTestWithEnumeratorPasses()
+    public IEnumerator WinCondition_CheckWinCondition_WithUnexpectedValue_ShouldReturnFalse()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        // Configuración de prueba
+        GameObject gameObj = new GameObject();
+        WinCondition winCondition = gameObj.AddComponent<WinCondition>();
+        HanoiDisk hanoiDisk = CreateDiskWithSize(3); // Crear un disco con un tamaño diferente al esperado
+        winCondition.expectedValue = 5;
+
+        // Ejecutar la prueba
+        bool result = winCondition.CheckWinCondition();
+
+        // Verificar el resultado
+        Assert.IsFalse(result);
+
         yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator WinCondition_CheckWinCondition_WithNoDisk_ShouldReturnFalse()
+    {
+        // Configuración de prueba
+        GameObject gameObj = new GameObject();
+        WinCondition winCondition = gameObj.AddComponent<WinCondition>();
+        winCondition.expectedValue = 5;
+
+        // Ejecutar la prueba
+        bool result = winCondition.CheckWinCondition();
+
+        // Verificar el resultado
+        Assert.IsFalse(result);
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator ConditionCheck_CheckCondition_WithDiskAbove_ShouldReturnTrue()
+    {
+        // Configuración de prueba
+        GameObject gameObj = new GameObject();
+        ConditionCheck conditionCheck = gameObj.AddComponent<ConditionCheck>();
+        HanoiDisk hanoiDiskAbove = CreateDiskWithSize(5); // Crear un disco con un tamaño superior
+        conditionCheck.transform.position = Vector3.zero; // Asegurarse de que la posición de la condición esté en cero
+
+        // Ejecutar la prueba
+        bool result = conditionCheck.CheckCondition(3);
+
+        // Verificar el resultado
+        Assert.IsFalse(result);
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator ConditionCheck_CheckCondition_WithDiskBelow_ShouldReturnFalse()
+    {
+        // Configuración de prueba
+        GameObject gameObj = new GameObject();
+        ConditionCheck conditionCheck = gameObj.AddComponent<ConditionCheck>();
+        HanoiDisk hanoiDiskBelow = CreateDiskWithSize(3); // Crear un disco con un tamaño inferior
+        conditionCheck.transform.position = Vector3.zero; // Asegurarse de que la posición de la condición esté en cero
+
+        // Ejecutar la prueba
+        bool result = conditionCheck.CheckCondition(5);
+
+        // Verificar el resultado
+        Assert.IsFalse(result);
+
+        yield return null;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Limpieza de recursos si es necesario.
+    }
+
+    // Función de utilidad para crear un disco con un tamaño específico
+    private HanoiDisk CreateDiskWithSize(int size)
+    {
+        GameObject diskObj = new GameObject();
+        HanoiDisk hanoiDisk = diskObj.AddComponent<HanoiDisk>();
+        hanoiDisk.size = size;
+        diskObj.tag = "Disk";
+        return hanoiDisk;
     }
 }
