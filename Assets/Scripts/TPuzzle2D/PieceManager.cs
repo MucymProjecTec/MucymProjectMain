f5e064575560a5b2750ada875256e2e030c29511
Alejandr24 <105420129+Alejandr24@users.noreply.github.com>
2024-05-07 13:40:19 -0600

    TPuzzle 2D listo
    - Juego de puzzle 2D terminado

---
+++ Assets/Scripts/TPuzzle2D/PieceManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceManager : MonoBehaviour
+{
+    [SerializeField] public PiecesIdentify pieceData;
+
+}
